1e627a76dab6eb6c5398f4c63614d802c29ab9a6
mensur056 <mansur.sarxanov@gmail.com>
2022-03-17 00:11:40 +0100

    Python

--- python_1-day.py
+++ python_1-day.py
@@ -1,2 +1,4 @@
-ad =input('you have to add the name: ')
-print(ad)
+name=input("you have to add the name: ")
+surmame=input('you can add the surname: ')
+age=int(input('you can add the age'))
+print(name+''+surmame+''+str(age))
